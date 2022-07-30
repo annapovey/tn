936ee0f87f720ee15f0054387774882836f32aaf
Anna Povey <annarobot4@gmail.com>
2022-07-29 23:18:04 -0700

    import pytest

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ def basic_test():
     a1 = "thousand"
     assert(tts_norm(t1)==a1)
 
-def currency_test():
-    t2 = "$3.9 billion"
-    a2 = "three point nine billion dollars"
-    assert(tts_norm(t2)==a2)
+# def currency_test():
+#     t2 = "$3.9 billion"
+#     a2 = "three point nine billion dollars"
+#     assert(tts_norm(t2)==a2)
 # t3 = "75% of children have a life expectancy of under 18."
 # a3 = "seventy five percent of children have a life expectancy of under eighteen"
 #assert(tts_norm(t3)==a3)
