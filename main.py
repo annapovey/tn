a84be75cf4ba74e3cef95e28996861033eb2e860
Anna Povey <annarobot4@gmail.com>
2022-08-05 19:02:39 -0700

    making radio buttons

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import streamlit as st
 from en_norm.__init__ import tts_norm
 form = st.form(key = 'my_form')
 text_input = form.text_input(label = 'input text here')
+rb1 = form.radio("keep punctuation?", (True))
+rb2 = form.radio("make text uppercase?", (True))
 submit_button = form.form_submit_button(label = 'submit')
-form2 = st.form(key = 'my_form2')
-text_input2 = form2.text_input(label = 'keep punctuation? input \'True\' or \'False\'')
-submit_button2 = form2.form_submit_button(label = 'submit')
-if submit_button and submit_button2:
-    st.write(tts_norm(text_input, bool(text_input2)))
+# form2 = st.form(key = 'my_form2')
+# text_input2 = form2.text_input(label = 'keep punctuation? input \'True\' or \'False\'')
+# submit_button2 = form2.form_submit_button(label = 'submit')
+# if submit_button and submit_button2:
+st.write(tts_norm(text_input))
