6afaa1aedbdc9391c574eddc413ec20d709f9c8e
Anna Povey <annarobot4@gmail.com>
2022-08-05 19:10:15 -0700

    header

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from en_norm.__init__ import tts_norm
-st.title("Text Normalization for Language Models")
+st.header("Text Normalization for Language Models")
 form = st.form(key = 'my_form')
 text_input = form.text_input(label = 'input text here')
 cb1 = form.checkbox("keep punctuation")
