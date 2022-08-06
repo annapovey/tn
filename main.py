0f83e7f02802743c53575e2b504ddd8f38e370e3
Anna Povey <annarobot4@gmail.com>
2022-08-05 17:43:58 -0700

    making punc button

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ from en_norm.__init__ import tts_norm
 form = st.form(key = 'my_form')
 text_input = form.text_input(label = 'input text here')
 submit_button = form.form_submit_button(label = 'submit')
-st.write(tts_norm(text_input, True))
+form2 = st.form(key = 'my_form')
+text_input2 = form2.text_input(label = 'keep punctuation? input \'True\' or \'False\'')
+submit_button2 = form2.form_submit_button(label = 'submit')
+st.write(tts_norm(text_input, bool(text_input2)))
