c110a4b87e9bb75bed140763e9c7843342c4b452
Anna Povey <annarobot4@gmail.com>
2022-08-05 17:40:44 -0700

    testing punctuation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
 from en_norm.__init__ import tts_norm
-title = st.text_input('text normalizer', 'input text')
-st.write(tts_norm(title, True))
+form = st.form(key = 'my_form')
+text_input = form.text_input(label = 'input text here')
+submit_button = form.form_submit_button(label = 'submit')
+st.write(tts_norm(text_input, True))
