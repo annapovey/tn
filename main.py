b5253775a7df1b8ed676a58f041a2c742acc9a10
Anna Povey <annarobot4@gmail.com>
2022-08-07 16:56:37 -0700

    footer

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from en_norm.__init__ import tts_norm
 
 logging.basicConfig(filename = 'maininfo.log', level = logging.INFO, format = '%(asctime)s:%(levelname)s:%(message)s')
 st.header("Text Normalization for ASR")
+st.footer("Mimicks libri speech text normalization")
 form = st.form(key = 'my_form')
 text_input = form.text_input(label = 'input text here')
 form.write("example: $3.9 billion -> three point nine billion dollars")
