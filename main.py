59ff73ac250b466d1aa8ee02830ab56ddf9c274b
Anna Povey <annarobot4@gmail.com>
2022-08-07 17:39:21 -0700

    footer

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from en_norm.__init__ import tts_norm
 
 logging.basicConfig(filename = 'maininfo.log', level = logging.INFO, format = '%(asctime)s:%(levelname)s:%(message)s')
 st.header("Text Normalization for ASR")
-st.caption("Mimicks libri speech text normalization\n-numbers to words\n - CNN -> C N N\n - removes punctuation\n - mimicks speech for dates")
+st.caption("Mimicks libri speech text normalization\n -numbers to words\n -CNN -> C N N\n -removes punctuation\n -mimicks speech for dates")
 form = st.form(key = 'my_form')
 text_input = form.text_input(label = 'input text here')
 form.caption("example: $3.9 billion -> three point nine billion dollars")
