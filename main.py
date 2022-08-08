8715051a8bdddc0d7ca4f4476f48fbfaf000f103
Anna Povey <annarobot4@gmail.com>
2022-08-07 17:00:08 -0700

    footer

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from en_norm.__init__ import tts_norm
 
 logging.basicConfig(filename = 'maininfo.log', level = logging.INFO, format = '%(asctime)s:%(levelname)s:%(message)s')
 st.header("Text Normalization for ASR")
-st.footer("Mimicks libri speech text normalization")
 form = st.form(key = 'my_form')
 text_input = form.text_input(label = 'input text here')
 form.write("example: $3.9 billion -> three point nine billion dollars")
@@ -20,3 +19,4 @@ try:
     st.write(tts_norm(text_input, cb1, cb2))
 except:
     logging.info(text_input)
+st.caption("Mimicks libri speech text normalization")
